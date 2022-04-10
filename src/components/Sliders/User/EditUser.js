e587fcd95a91e5cd9f695de04d70eef2c037098d
Slokyy <slobodancvetkovic995@gmail.com>
2022-04-10 22:21:28 +0200

    Added update state on EditUser

--- src/components/Sliders/User/EditUser.js
+++ src/components/Sliders/User/EditUser.js
@@ -5,286 +5,288 @@ import { useUserState } from "../../../pages/Users/UserContext";
 import "./User.css";
 
 const EditUser = () => {
-	const [editData, setEditData] = useState([]);
-	const [isPosting, setIsPosting] = useState(false);
-	const { activeUserId, editUserSlide, setEditUserSlide } = useUserState();
-	const navigate = useNavigate();
+  const [editData, setEditData] = useState([]);
+  const [isPosting, setIsPosting] = useState(false);
+  const { activeUserId, editUserSlide, setEditUserSlide } = useUserState();
+  const navigate = useNavigate();
+  const { refetch, setRefetch } = useGlobalState();
 
-	useEffect(() => {
-		if (activeUserId) {
-			fetch(`http://localhost:8000/users/${activeUserId}`)
-				.then((res) => res.json())
-				.then((data) => {
-					setEditData(data);
-				})
-				.catch((err) => console.log("EditFetchErr: ", err));
-		}
-	}, [activeUserId]);
+  useEffect(() => {
+    if (activeUserId) {
+      fetch(`http://localhost:8000/users/${activeUserId}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setEditData(data);
+        })
+        .catch((err) => console.log("EditFetchErr: ", err));
+    }
+  }, [activeUserId]);
 
-	const handleEditUserSubmit = (e) => {
-		e.preventDefault();
-		setIsPosting(true);
-		fetch(`http://localhost:8000/users/${activeUserId}`, {
-			method: "PUT",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(editData),
-		})
-			.then(() => setIsPosting(false))
-			.then(() => {
-				setTimeout(() => {
-					// console.log("Edited user");
-					// navigate("/");
-					setEditUserSlide(!editUserSlide);
-				}, 1000);
-			});
-	};
-	return (
-		<section
-			className={
-				editUserSlide
-					? "slide-container edit-user-container active"
-					: "slide-container edit-user-container"
-			}
-			id="edit-user-container"
-		>
-			<span
-				className="icon icon-md icon-ex  js-btn-close slide-ex"
-				onClick={() => setEditUserSlide(!editUserSlide)}
-			></span>
-			<form
-				action="#!"
-				className="slide-form flex"
-				onSubmit={handleEditUserSubmit}
-			>
-				<h2>Edit user: {editData?.first_name}</h2>
+  const handleEditUserSubmit = (e) => {
+    e.preventDefault();
+    setIsPosting(true);
+    fetch(`http://localhost:8000/users/${activeUserId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(editData),
+    })
+      .then(() => setIsPosting(false))
+      .then(() => {
+        setTimeout(() => {
+          // console.log("Edited user");
+          // navigate("/");
+          setRefetch(!refetch);
+          setEditUserSlide(!editUserSlide);
+        }, 1000);
+      });
+  };
+  return (
+    <section
+      className={
+        editUserSlide
+          ? "slide-container edit-user-container active"
+          : "slide-container edit-user-container"
+      }
+      id="edit-user-container"
+    >
+      <span
+        className="icon icon-md icon-ex  js-btn-close slide-ex"
+        onClick={() => setEditUserSlide(!editUserSlide)}
+      ></span>
+      <form
+        action="#!"
+        className="slide-form flex"
+        onSubmit={handleEditUserSubmit}
+      >
+        <h2>Edit user: {editData?.first_name}</h2>
 
-				<div className="form-group flex">
-					<img
-						src={editData?.avatar?.image_path}
-						className="profile-picture"
-						alt="Loreta Ipsum"
-					></img>
-					<label
-						className="label-image shadow-me flex"
-						htmlFor="add-user-img"
-					></label>
+        <div className="form-group flex">
+          <img
+            src={editData?.avatar?.image_path}
+            className="profile-picture"
+            alt="Loreta Ipsum"
+          ></img>
+          <label
+            className="label-image shadow-me flex"
+            htmlFor="add-user-img"
+          ></label>
 
-					<input
-						type="file"
-						name="add-user-img"
-						className="add-user-img"
-						id="add-user-img"
-						hidden
-					/>
-				</div>
-				<div className="form-group">
-					<input
-						type="text"
-						id="user-name"
-						className="shadow-me input-text-gray"
-						name="user-name"
-						defaultValue={editData?.first_name}
-						onChange={(e) =>
-							setEditData({ ...editData, first_name: e.target.value })
-						}
-						required
-					/>
-					<label htmlFor="user-email" className="label-name">
-						<span className="content-name">Name:</span>
-					</label>
-				</div>
+          <input
+            type="file"
+            name="add-user-img"
+            className="add-user-img"
+            id="add-user-img"
+            hidden
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            id="user-name"
+            className="shadow-me input-text-gray"
+            name="user-name"
+            defaultValue={editData?.first_name}
+            onChange={(e) =>
+              setEditData({ ...editData, first_name: e.target.value })
+            }
+            required
+          />
+          <label htmlFor="user-email" className="label-name">
+            <span className="content-name">Name:</span>
+          </label>
+        </div>
 
-				<div className="form-group">
-					<input
-						type="text"
-						id="user-lname"
-						name="user-lname"
-						className="shadow-me input-text-gray"
-						defaultValue={editData?.last_name}
-						onChange={(e) =>
-							setEditData({ ...editData, last_name: e.target.value })
-						}
-						required
-					/>
-					<label htmlFor="user-lname" className="label-name">
-						<span className="content-name">Last name:</span>
-					</label>
-				</div>
+        <div className="form-group">
+          <input
+            type="text"
+            id="user-lname"
+            name="user-lname"
+            className="shadow-me input-text-gray"
+            defaultValue={editData?.last_name}
+            onChange={(e) =>
+              setEditData({ ...editData, last_name: e.target.value })
+            }
+            required
+          />
+          <label htmlFor="user-lname" className="label-name">
+            <span className="content-name">Last name:</span>
+          </label>
+        </div>
 
-				<div className="form-group">
-					<input
-						type="text"
-						id="user-email"
-						className="shadow-me input-text-gray"
-						name="user-email"
-						defaultValue={editData?.email}
-						onChange={(e) =>
-							setEditData({ ...editData, email: e.target.value })
-						}
-						required
-					/>
-					<label htmlFor="user-email" className="label-name">
-						<span className="content-name">Email:</span>
-					</label>
-				</div>
+        <div className="form-group">
+          <input
+            type="text"
+            id="user-email"
+            className="shadow-me input-text-gray"
+            name="user-email"
+            defaultValue={editData?.email}
+            onChange={(e) =>
+              setEditData({ ...editData, email: e.target.value })
+            }
+            required
+          />
+          <label htmlFor="user-email" className="label-name">
+            <span className="content-name">Email:</span>
+          </label>
+        </div>
 
-				<div className="form-group">
-					<input
-						type="text"
-						id="user-phone"
-						className="shadow-me input-text-gray"
-						name="user-phone"
-						defaultValue={editData?.phone}
-						onChange={(e) =>
-							setEditData({ ...editData, phone: e.target.value })
-						}
-						required
-					/>
-					<label htmlFor="user-phone" className="label-name">
-						<span className="content-name">Phone:</span>
-					</label>
-				</div>
+        <div className="form-group">
+          <input
+            type="text"
+            id="user-phone"
+            className="shadow-me input-text-gray"
+            name="user-phone"
+            defaultValue={editData?.phone}
+            onChange={(e) =>
+              setEditData({ ...editData, phone: e.target.value })
+            }
+            required
+          />
+          <label htmlFor="user-phone" className="label-name">
+            <span className="content-name">Phone:</span>
+          </label>
+        </div>
 
-				<div className="form-group">
-					<input
-						type="text"
-						id="user-street"
-						className="shadow-me input-text-gray"
-						name="user-street"
-						defaultValue={editData?.street}
-						onChange={(e) =>
-							setEditData({ ...editData, street: e.target.value })
-						}
-						required
-					/>
-					<label htmlFor="user-street" className="label-name">
-						<span className="content-name">Street:</span>
-					</label>
-				</div>
+        <div className="form-group">
+          <input
+            type="text"
+            id="user-street"
+            className="shadow-me input-text-gray"
+            name="user-street"
+            defaultValue={editData?.street}
+            onChange={(e) =>
+              setEditData({ ...editData, street: e.target.value })
+            }
+            required
+          />
+          <label htmlFor="user-street" className="label-name">
+            <span className="content-name">Street:</span>
+          </label>
+        </div>
 
-				<div className="form-group">
-					<input
-						type="text"
-						id="user-city"
-						className="shadow-me input-text-gray"
-						name="user-city"
-						defaultValue={editData?.city}
-						onChange={(e) => setEditData({ ...editData, city: e.target.value })}
-						required
-					/>
-					<label htmlFor="user-city" className="label-name">
-						<span className="content-name">City:</span>
-					</label>
-				</div>
+        <div className="form-group">
+          <input
+            type="text"
+            id="user-city"
+            className="shadow-me input-text-gray"
+            name="user-city"
+            defaultValue={editData?.city}
+            onChange={(e) => setEditData({ ...editData, city: e.target.value })}
+            required
+          />
+          <label htmlFor="user-city" className="label-name">
+            <span className="content-name">City:</span>
+          </label>
+        </div>
 
-				<div className="form-group">
-					<input
-						type="text"
-						id="user-ptt"
-						className="shadow-me input-text-gray"
-						name="user-ptt"
-						defaultValue={editData?.ptt}
-						onChange={(e) => setEditData({ ...editData, ptt: e.target.value })}
-						required
-					/>
-					<label htmlFor="user-ptt" className="label-name">
-						<span className="content-name">PTT:</span>
-					</label>
-				</div>
+        <div className="form-group">
+          <input
+            type="text"
+            id="user-ptt"
+            className="shadow-me input-text-gray"
+            name="user-ptt"
+            defaultValue={editData?.ptt}
+            onChange={(e) => setEditData({ ...editData, ptt: e.target.value })}
+            required
+          />
+          <label htmlFor="user-ptt" className="label-name">
+            <span className="content-name">PTT:</span>
+          </label>
+        </div>
 
-				<div className="form-group">
-					<input
-						type="text"
-						id="user-country"
-						className="shadow-me input-text-gray"
-						name="user-country"
-						defaultValue={editData?.country}
-						onChange={(e) =>
-							setEditData({ ...editData, country: e.target.value })
-						}
-						required
-					/>
-					<label htmlFor="user-country" className="label-name">
-						<span className="content-name">Country:</span>
-					</label>
-				</div>
+        <div className="form-group">
+          <input
+            type="text"
+            id="user-country"
+            className="shadow-me input-text-gray"
+            name="user-country"
+            defaultValue={editData?.country}
+            onChange={(e) =>
+              setEditData({ ...editData, country: e.target.value })
+            }
+            required
+          />
+          <label htmlFor="user-country" className="label-name">
+            <span className="content-name">Country:</span>
+          </label>
+        </div>
 
-				<div className="form-group">
-					<input
-						type="text"
-						id="user-status"
-						className="shadow-me input-text-gray"
-						name="user-status"
-						defaultValue={editData?.status}
-						onChange={(e) =>
-							setEditData({ ...editData, status: e.target.value })
-						}
-						required
-					/>
-					<label htmlFor="user-status" className="label-name">
-						<span className="content-name">Status:</span>
-					</label>
-				</div>
+        <div className="form-group">
+          <input
+            type="text"
+            id="user-status"
+            className="shadow-me input-text-gray"
+            name="user-status"
+            defaultValue={editData?.status}
+            onChange={(e) =>
+              setEditData({ ...editData, status: e.target.value })
+            }
+            required
+          />
+          <label htmlFor="user-status" className="label-name">
+            <span className="content-name">Status:</span>
+          </label>
+        </div>
 
-				<div className="form-group">
-					<input
-						type="text"
-						id="user-role"
-						className="shadow-me input-text-gray"
-						name="user-role"
-						defaultValue={editData?.role}
-						onChange={(e) => setEditData({ ...editData, role: e.target.value })}
-						required
-					/>
-					<label htmlFor="user-role" className="label-name">
-						<span className="content-name">Role:</span>
-					</label>
-				</div>
+        <div className="form-group">
+          <input
+            type="text"
+            id="user-role"
+            className="shadow-me input-text-gray"
+            name="user-role"
+            defaultValue={editData?.role}
+            onChange={(e) => setEditData({ ...editData, role: e.target.value })}
+            required
+          />
+          <label htmlFor="user-role" className="label-name">
+            <span className="content-name">Role:</span>
+          </label>
+        </div>
 
-				<div className="form-group">
-					<input
-						type="text"
-						id="user-tekuci-rac"
-						className="shadow-me input-text-gray"
-						name="user-tekuci-rac"
-						defaultValue={editData?.tekuci_racun}
-						onChange={(e) =>
-							setEditData({ ...editData, tekuci_racun: e.target.value })
-						}
-						required
-					/>
-					<label htmlFor="user-tekuci-rac" className="label-name">
-						<span className="content-name">Tekući račun:</span>
-					</label>
-				</div>
+        <div className="form-group">
+          <input
+            type="text"
+            id="user-tekuci-rac"
+            className="shadow-me input-text-gray"
+            name="user-tekuci-rac"
+            defaultValue={editData?.tekuci_racun}
+            onChange={(e) =>
+              setEditData({ ...editData, tekuci_racun: e.target.value })
+            }
+            required
+          />
+          <label htmlFor="user-tekuci-rac" className="label-name">
+            <span className="content-name">Tekući račun:</span>
+          </label>
+        </div>
 
-				<div className="form-group input flex">
-					<input
-						type="button"
-						className="btn js-btn-close shadow-me"
-						onClick={() => setEditUserSlide(!editUserSlide)}
-						defaultValue="Cancel"
-					/>
-					{!isPosting && (
-						<input
-							type="submit"
-							className="btn btn-small btn-blue shadow-me"
-							defaultValue="Edit user"
-						/>
-					)}
-					{isPosting && (
-						<input
-							type="submit"
-							className="btn btn-small btn-blue shadow-me"
-							defaultValue="Editing user..."
-						/>
-					)}
-				</div>
-			</form>
-		</section>
-	);
+        <div className="form-group input flex">
+          <input
+            type="button"
+            className="btn js-btn-close shadow-me"
+            onClick={() => setEditUserSlide(!editUserSlide)}
+            defaultValue="Cancel"
+          />
+          {!isPosting && (
+            <input
+              type="submit"
+              className="btn btn-small btn-blue shadow-me"
+              defaultValue="Edit user"
+            />
+          )}
+          {isPosting && (
+            <input
+              type="submit"
+              className="btn btn-small btn-blue shadow-me"
+              defaultValue="Editing user..."
+            />
+          )}
+        </div>
+      </form>
+    </section>
+  );
 };
 
 export default EditUser;
--- src/pages/context.js
+++ src/pages/context.js
@@ -2,89 +2,90 @@ import React, { useState, useEffect, useContext } from "react";
 const AppContext = React.createContext(null);
 
 const AppProvider = ({ children }) => {
-	const [pathname, setPathname] = useState("");
-	const [isLogedIn, setIsLogedIn] = useState(true);
+  const [pathname, setPathname] = useState("");
+  const [isLogedIn, setIsLogedIn] = useState(true);
 
-	const [loggedUser, setLoggedUser] = useState([]);
-	const [data, setData] = useState([]);
-	const [addUserSlide, setAddUserSlide] = useState(false);
-	const [isLoading, setIsLoading] = useState(true);
-	const [refetch, setRefetch] = useState(false);
-	const fetchUsers = (url) => {
-		fetch(url, {
-			method: "GET",
-		})
-			.then((res) => {
-				if (!res.ok) {
-					setIsLoading(false);
-					throw Error("OK: Bad response", res.status);
-				}
-				return res.json();
-			})
-			.then((data) => {
-				setData(data);
-				setIsLoading(false);
-				console.log(data);
-			})
-			.catch((err) => {
-				console.log("Error message: ", err);
-				setIsLoading(false);
-			});
-	};
+  const [loggedUser, setLoggedUser] = useState([]);
+  const [data, setData] = useState([]);
+  const [addUserSlide, setAddUserSlide] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [refetch, setRefetch] = useState(false);
 
-	useEffect(() => {
-		if (isLogedIn && localStorage.getItem("user")) {
-			let currentUser = JSON.parse(localStorage.getItem("user"));
-			fetch(`http://localhost:8000/users/${currentUser.id}`)
-				.then((res) => {
-					if (!res.ok) {
-						throw Error("OK: Bad Fetch Logged User", res.status);
-					}
-					return res.json();
-				})
-				.then((data) => {
-					setLoggedUser(data);
-				})
-				.catch((err) => {
-					console.log("Error fetching logged user: ", err);
-				});
-		}
-	}, [pathname]);
+  const fetchUsers = (url) => {
+    fetch(url, {
+      method: "GET",
+    })
+      .then((res) => {
+        if (!res.ok) {
+          setIsLoading(false);
+          throw Error("OK: Bad response", res.status);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+        setIsLoading(false);
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log("Error message: ", err);
+        setIsLoading(false);
+      });
+  };
 
-	const handleLocationPath = () => {
-		setPathname(window.location.pathname.replace(/^\/|\/$/g, ""));
-	};
+  useEffect(() => {
+    if (isLogedIn && localStorage.getItem("user")) {
+      let currentUser = JSON.parse(localStorage.getItem("user"));
+      fetch(`http://localhost:8000/users/${currentUser.id}`)
+        .then((res) => {
+          if (!res.ok) {
+            throw Error("OK: Bad Fetch Logged User", res.status);
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setLoggedUser(data);
+        })
+        .catch((err) => {
+          console.log("Error fetching logged user: ", err);
+        });
+    }
+  }, [pathname]);
 
-	return (
-		<AppContext.Provider
-			value={{
-				data,
-				setData,
-				isLoading,
-				setIsLoading,
-				pathname,
-				setPathname,
-				handleLocationPath,
-				fetchUsers,
-				addUserSlide,
-				setAddUserSlide,
-				refetch,
-				setRefetch,
-				loggedUser,
-				setLoggedUser,
-				isLogedIn,
-				setIsLogedIn,
-			}}
-		>
-			{children}
-		</AppContext.Provider>
-	);
+  const handleLocationPath = () => {
+    setPathname(window.location.pathname.replace(/^\/|\/$/g, ""));
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        data,
+        setData,
+        isLoading,
+        setIsLoading,
+        pathname,
+        setPathname,
+        handleLocationPath,
+        fetchUsers,
+        addUserSlide,
+        setAddUserSlide,
+        refetch,
+        setRefetch,
+        loggedUser,
+        setLoggedUser,
+        isLogedIn,
+        setIsLogedIn,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 // Custom Hook
 // https://stackoverflow.com/questions/66747556/react-js-error-uselocation-may-be-used-only-in-the-context-of-a-router-com
 const useGlobalState = () => {
-	return useContext(AppContext);
+  return useContext(AppContext);
 };
 
 export { AppContext, AppProvider, useGlobalState };
